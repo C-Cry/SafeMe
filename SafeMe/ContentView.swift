da60590720de6b3c1e09d4efeca59f6e720be9ae
C-Cry <137468085+C-Cry@users.noreply.github.com>
2024-06-18 15:44:46 +0900

    Update ContentView.swift

--- SafeMe/ContentView.swift
+++ SafeMe/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Gang Type shit")
+            Text("SEX")
         }
         .padding()
     }
