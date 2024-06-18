7630ce12b8d309a57bf137b924aa69839ae2d271
C-Cry <137468085+C-Cry@users.noreply.github.com>
2024-06-18 15:27:53 +0900

    Initial Commit

---
+++ SafeMe/SafeMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeMeApp.swift
+//  SafeMe
+//
+//  Created by Munkh-Orgil Byambayar on 2024/06/18.
+//
+
+import SwiftUI
+
+@main
+struct SafeMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
